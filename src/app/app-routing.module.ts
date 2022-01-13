6308eb8e4bc213c82761a21f87e6a46edccd3c1e
Jung Kyu Yang <jungkyuyang@yahoo.com>
2022-01-13 10:04:37 -0800

    Added 404 Redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MerchComponent } from './merch/merch.component';
 import { MerchDetailComponent } from './merch-detail/merch-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 import { MusicComponent } from './music/music.component';
 import { TourComponent } from './tour/tour.component';
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", redirectTo: "/home" },
+  { path: "", pathMatch: "full", redirectTo: "/" },
   { path: "home", component: HomeComponent },
   { path: "merch", component: MerchComponent },
+  { path: "*", component: PageNotFoundComponent },
   /*
   { path: "merch", 
     component: MerchComponent, 
