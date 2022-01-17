b9a639f29263289c97f4dea32546f8f16fcabcc5
Jung Kyu Yang <jungkyuyang@yahoo.com>
2022-01-16 19:08:58 -0800

    made CSS button updates

---
+++ src/app/home/home.component.js
@@ -0,0 +1,19 @@
+/*alert();
+
+window.addEventListener("resize", getDisplayHeight);
+
+    function getDisplayHeight() {
+        alert();
+        if(window.innerHeight/window.innerWidth > .4 ){
+        console.log("yes");
+        document.getElementById("carousel").style.height = "100vh";
+       // let elem = document.getElementById('carousel');
+       // let propertyValue = elem.style['width: 100px'];
+          //document.getElementsByTagName("carousel")[0].setAttribute("style", "height: 100vh");
+         // document.getElementsByTagName("carousel")[0].setAttribute("style", "width: auto")
+      }
+      else {
+        console.log("no");
+      }
+    }
+    */
